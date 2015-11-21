79b4e36dad0cdbebfdfc9834804db01811b49fe4
juliode2000 <juliode2000@yahoo.com.mx>
2015-11-21 15:51:23 -0600

    Conexión Internet Tarea 1

--- demoConexionIOS/ViewController.swift
+++ demoConexionIOS/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBusqueda: UITextField!
+    @IBOutlet weak var textViewResultado: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func sincrono(){
+        let url$ = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textBusqueda.text!
+        let url = NSURL(string: url$)
+        let datos:NSData?=NSData(contentsOfURL: url!)
+        let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        
+        textViewResultado.text = texto! as String
+    print(url)
+    
+    }
+    @IBAction func buscarButon(sender: AnyObject) {
+        sincrono()
+        
+    }
+       @IBAction func clearButton(sender: AnyObject) {
+        textBusqueda.text = ""
+    }
 
 }
 
