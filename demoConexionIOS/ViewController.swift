2d25685df3780c1fc3d2f11c86a4c7d0cfb2119f
juliode2000 <juliode2000@yahoo.com.mx>
2015-11-21 15:23:19 -0600

    Initial Commit

---
+++ demoConexionIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoConexionIOS
+//
+//  Created by Julio Alfonso De Leon Razo on 21/11/15.
+//  Copyright © 2015 Julio Alfonso De Leon Razo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ demoConexionIOSTests/demoConexionIOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  demoConexionIOSTests.swift
+//  demoConexionIOSTests
+//
+//  Created by Julio Alfonso De Leon Razo on 21/11/15.
+//  Copyright © 2015 Julio Alfonso De Leon Razo. All rights reserved.
+//
+
+import XCTest
+@testable import demoConexionIOS
+
+class demoConexionIOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
